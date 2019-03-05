b48a67e2e5c8216ba864a4c6de22d032cda649bb
roy4801 <a82611141@gmail.com>
2019-03-05 11:34:28 +0800

    Add script for gen data_total

---
+++ samplecode/result/gen_data_total.py
@@ -0,0 +1,19 @@
+
+s = ['Alternative', 'Iterator', 'Simple']
+f = ['data_len', 'data_line']
+
+def main():
+	for i in s:
+		i += '/'
+		for j in f:
+			try:
+				with open(i + j + '.txt') as inp, open(i + j + '_total.txt', 'w') as outp:
+					data = [x.split() for x in inp.readlines()]
+					for ii in range(len(data)):
+						outp.write(data[ii][0] + ' ')
+						outp.write('{:.7f}\n'.format(float(data[ii][1]) + float(data[ii][2]) + float(data[ii][3])))
+			except FileNotFoundError as e:
+				print('Warning: {}'.format(e))
+				pass
+if __name__ == '__main__':
+	main()
