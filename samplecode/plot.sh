448c9b2b87d05a14af441950e552fa2c30ebdda9
halloworld <william31212@gmail.com>
2019-03-05 13:22:31 +0800

    new input,output compare & modify makefile plot.sh

--- samplecode/plot.sh
+++ samplecode/plot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Plot Alternative
+
 pushd result/Alternative/ > /dev/null
 echo Ploting Alternative
 gnuplot plot_len.gp
@@ -19,4 +20,38 @@ pushd result/Simple/ > /dev/null
 echo Ploting Simple
 gnuplot plot_len.gp
 gnuplot plot_line.gp
+popd > /dev/null
+
+# sort compare
+pushd result/ > /dev/null
+echo Ploting sort
+gnuplot sort_compare_len.gp
+popd > /dev/null
+
+pushd result/ > /dev/null
+echo Ploting sort
+gnuplot sort_compare_line.gp
+popd > /dev/null
+
+# input compare
+pushd result/ > /dev/null
+echo Ploting input
+gnuplot input_compare_len.gp
+popd > /dev/null
+
+pushd result/ > /dev/null
+echo Ploting input
+gnuplot input_compare_line.gp
+popd > /dev/null
+
+
+# output compare
+pushd result/ > /dev/null
+echo Ploting output
+gnuplot output_compare_len.gp
+popd > /dev/null
+
+pushd result/ > /dev/null
+echo Ploting output
+gnuplot output_compare_line.gp
 popd > /dev/null
