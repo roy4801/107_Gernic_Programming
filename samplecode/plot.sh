605dcf3828dbcf42948bf2706aeb74563865581b
roy4801 <a82611141@gmail.com>
2019-03-05 10:54:17 +0800

    Change makefile labels

--- samplecode/plot.sh
+++ samplecode/plot.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 # Plot Alternative
-pushd result/Alternative/
+pushd result/Alternative/ > /dev/null
+echo Ploting Alternative
 gnuplot plot_len.gp
 gnuplot plot_line.gp
-popd
+popd > /dev/null
 
 # Plot Iterator
-pushd result/Iterator/
+pushd result/Iterator/ > /dev/null
+echo Ploting Iterator
 gnuplot plot_len.gp
 gnuplot plot_line.gp
-popd
+popd > /dev/null
 
 # Plot Simple
-pushd result/Simple/
+pushd result/Simple/ > /dev/null
+echo Ploting Simple
 gnuplot plot_len.gp
 gnuplot plot_line.gp
-popd
+popd > /dev/null
